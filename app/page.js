b90fec5bad102a97cc6f8e0fb767dcc3763f3524
Nagakburos <wansanice@proton.me>
2024-03-04 16:09:42 -0300

    Landing Page: Design a Showcase

--- app/page.js
+++ app/page.js
@@ -7,14 +7,14 @@ export default function Home() {
         <div className="col-span-7 place-self-center mr-auto">
           <h1 className="masx-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl">
             Crie seu quartos dos sonhos <br className="max-md:hidden" />
-            <span className="text-center purple_gradient">usando AI.</span>
+            <span className="text-center purple_gradient">usando IA.</span>
           </h1>
           <p className="hero_desc">
             Faça upLoad da sua foto, escolha um modo e escolha entre mais de 40
             estilos.
             <strong>
               Reimagine o Design Interior de Qualquer Casa, ou Jardin usando o
-              RefreshDesign AI.
+              RefreshDesign IA.
             </strong>
           </p>
           <button type="button" className="mt-8 black_btn">
@@ -35,10 +35,35 @@ export default function Home() {
         </div>
       </div>
       <div className="text-center max-w-3xl mb-4 text-3xl font-extrabold tracking-tighter md:text-4xl mx-auto">
-      Design de Paisagismo e Interiores Criados com AI
+      Design de Paisagismo e Interiores Criados com IA
       </div>
       <div className="mt-5 text-lg text-center font-light text-gray-500 max-w-4xl md:text-lg mx-auto">
       Nossa inteligência artificial automaticamente reprojeta ambientes externos e internos, considerando elementos arquitetônicos como gramados, pátios, piscinas e mobiliário, eletrodomésticos externos, desde bancos de jardim até instalações para churrasco.
+      </div>
+      <div className="mt-8 px-4 py-8 max-w-screen-xl grid grid-cols-9 gap-4 w-full mx-auto">
+{/*Image exemple*/}
+        <div className="col-span-4">
+          <div className="h-64 sm:h-96 w-full aspect-video bg-gray-100 relative">
+            <Image className="rounded-lg object-cover" alt="photo" src="/assets/images/original-pic.jpg" fill />
+          </div>
+        </div>
+{/*Arrow*/}
+        <div className="col-span-1">
+          <div className="h-64 sm:h-96 w-full flex justify-center  items-center">
+            <Image 
+            alt="photo" 
+            src="/assets/images/arrow.png"
+            width={30}
+            height={30}
+            />
+          </div>
+        </div>
+{/*Image Result*/}
+        <div className="col-span-4">
+          <div className="h-64 sm:h-96 w-full aspect-video bg-gray-100 relative">
+            <Image className="rounded-lg object-cover" alt="photo" src="/assets/images/generated-pic.png" fill />
+          </div>
+        </div>
       </div>
     </div>
   );
