465d8b3597776393175ae0f9812f9c8254da9319
Nagakburos <wansanice@proton.me>
2024-03-08 20:54:23 -0300

    NextAuth  implement
    9. Setting Up NextAuth For Our Application

--- app/api/[...nextauth]/route.js
+++ app/api/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/utils/auth-option";
 
-const handler = NextAuth(authOptionsuthOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST};
--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={font.className}>
         <Provider>
-        <Navbar />
-        {children}
+          <Navbar />
+          {children}
         </Provider>
       </body>
     </html>
